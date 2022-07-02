e282fcbb673d254632d062ebd7ae081d874048eb
Liam Bendix <48177481+B3ND1X@users.noreply.github.com>
2022-07-02 20:26:32 +0000

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
+start () {
 clear
 echo "Installing please wait..."
 sleep 2
@@ -13,10 +14,60 @@ mkdir /bin/timestone/
 mkdir /bin/timestone/backups
 mkdir /bin/timestone/tmp
 cp /home/*/timestone/* /bin/timestone/
+chmod +x /bin/timestone/install.sh
+chmod +x /bin/timestone/restore.sh
+chmod +x /bin/timestone/timestone.sh
+chmod +x /bin/timestone/install.sh
+chmod +x /home/*/timestone/timestone.sh
+chmod +x /home/*/timestone/uninstall.sh
+chomd +x /home/*/timestone/restore.sh
 cp /bin/timestone/timestone.service /lib/systemd/system
 sudo systemctl daemon-reload
 apt-get update
 apt-get install tar -y
+clear
+cleanup
+
+}
+
+cleanup () {
+while true; do
+    read -p "Time Stone has been moved to /bin/timestone. Would you like to clean up and remove Time Stone from your home directory?" yn
+    case $yn in
+        [Yy]* ) cleanYes; break;;
+        [Nn]* ) cleanNo;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+}
+
+cleanYes () { 
+clear
+sleep 1
+echo 'Okay, cleaning up... Please wait.'
+rm -rf /home/*/timestone
+echo 'Done.'
+sleep 1
 echo 'DONE!'
 sleep 2
-./timestone.sh
+exit 1
+}
+
+cleanNo () {
+clear
+sleep 1
+echo 'Okay, no cleanup will be made.'
+sleep 1
+echo 'Please wait while the installation finishes.'
+sleep 1 
+echo 'DONE!'
+sleep 2
+exit 1
+}
+
+
+targeted () {
+start
+}
+targeted
